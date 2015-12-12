9bcdc27fc81a7b1b0c046b8a62844bd4e06b31d8
adrian_rivero <adrirp149@yahoo.es>
2015-12-12 20:35:48 +0000

    Fader

---
+++ Assets/Scripts/fader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class fader : MonoBehaviour {
+
+	private Image fade;
+
+	// Use this for initialization
+	void Start () {
+
+		fade = this.GetComponent<Image> ();
+		CrossFade (true);
+	}
+
+	public void CrossFade(bool fadeIn)
+	{
+		if (fadeIn) {
+			fade.CrossFadeAlpha (0.0f, 1.0f, false);
+		} else {
+			fade.CrossFadeAlpha (1.0f, 1.0f, false);
+		}
+	}
+}
