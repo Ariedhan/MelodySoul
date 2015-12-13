9e092416b255ad5a4e4458b8aae56334c1bab6dd
ayose <ayoseftv@gmail.com>
2015-12-13 10:28:20 +0000

    escena corregida enemigo

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour 
+{
+	float t; 
+	public enum CameraState
+	{
+		follow,
+		goTo
+	}
+	public CameraState state;
+
+	// Use this for initialization
+	void Start () 
+	{
+		state = CameraState.follow;
+	}
+
+	IEnumerator GoTo(Vector3 v)
+	{		
+		t = 0f;
+
+		while (t < 1)
+		{
+			Debug.Log (state);
+			if (state != CameraState.goTo)
+				break;
+			Vector2 aux = new Vector2 (0, 0);
+			aux = Vector2.Lerp ((Vector2)transform.position,(Vector2)v, t); 
+			transform.position = new Vector3(aux.x,aux.y,transform.position.z); 
+
+			t += Time.deltaTime;
+			yield return 0;
+		} 	 
+	}
+
+	IEnumerator FollowObject(GameObject obj)
+	{
+		while (state == CameraState.follow) 
+		{
+			transform.position = new Vector3 (obj.transform.position.x, transform.position.y, transform.position.z);
+			yield return 0;
+		}
+	}
+
+	public void ChangeToFollow(GameObject obj)
+	{
+		state = CameraState.follow;
+		StartCoroutine (FollowObject(obj));
+	}
+
+	public void ChangeToGoTo(Vector3 target)
+	{
+		state = CameraState.goTo;
+		StartCoroutine (GoTo(target));
+	}
+
+	void Update()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour 
+{
+	LevelController lvl;
+	void Start()
+	{
+		lvl = GameObject.Find ("Level").GetComponent<LevelController> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player") 
+		{
+			lvl.PlayerDeath ();
+		}
+	}
+}
