5dc92ec079daf8c4883c7cf3bbf852b7f70d61c0
ayose <ayoseftv@gmail.com>
2015-12-13 16:24:31 +0000

    animator controller enemy and menu controller

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,17 +3,61 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour 
 {
-	LevelController lvl;
+	public enum EnemyState
+	{
+		idle,
+		attack,
+		death
+	}
+	public EnemyState state;
+	Animator anim;
+	int idleHash,attackHash,deathHash;
+	int attackTriggerHash,deathTriggerHash,idleTriggerHash;
+
 	void Start()
 	{
-		lvl = GameObject.Find ("Level").GetComponent<LevelController> ();
+		state = EnemyState.idle;
+		anim = GetComponent<Animator> ();
+		idleHash = Animator.StringToHash("Base Layer.Idle");
+		attackHash = Animator.StringToHash("Base Layer.Attack");
+		deathHash = Animator.StringToHash("Base Layer.Death");
+		attackTriggerHash = Animator.StringToHash("attackTrigger");
+		deathTriggerHash = Animator.StringToHash("deathTrigger");
+		idleTriggerHash = Animator.StringToHash("idleTrigger");
 	}
 
-	void OnTriggerEnter2D(Collider2D other)
+	public void EnemyIdle()
 	{
-		if (other.tag == "Player") 
-		{
-			lvl.PlayerDeath ();
-		}
+		anim.SetTrigger (idleTriggerHash);
 	}
+
+	public void EnemyAttack()
+	{
+		anim.SetTrigger (attackTriggerHash);
+	}
+
+	public void EnemyDeath()
+	{
+		anim.SetTrigger (deathTriggerHash);
+	}
+
+	void Update()
+	{
+		AnimatorStateInfo stateinfo =  anim.GetCurrentAnimatorStateInfo(0);
+		if (Input.GetKeyDown (KeyCode.A)) 
+		{
+			EnemyAttack ();
+		}	
+
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			EnemyDeath ();
+		}	
+
+		if (Input.GetKeyDown (KeyCode.I)) 
+		{
+			EnemyIdle ();
+		}	
+	}
+
 }
